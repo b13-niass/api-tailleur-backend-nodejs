11d4901c77888a5e30bf4391cfd539a006a66645
Cheikh Ibrahima Dieng <44117232+b13-niass@users.noreply.github.com>
2024-08-08 12:53:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ const app = express();
 const BASE_API = process.env.PREFIX_URI;
 const PORT = process.env.PORT
 
-// Création du token avec l'ID du compte
-// const token = createJWT({ id: '66b393e26493413deca60f24', role: 'tailleur'});
-
-// //Affichage du token dans la console
-// console.log("Token généré :", token);
-
-
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -32,5 +25,5 @@ app.use(`${BASE_API}/client`, clientRoutes);
 app.use(`${BASE_API}/tailleur`, tailleurRoutes);
 
 app.listen(PORT, () => {
-    console.log("server is listenning on port 4000....");
+    console.log("server is listenning on port 5000....");
 });
